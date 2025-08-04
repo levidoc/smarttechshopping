781ad00f3a3faee679b5330c344aad11eff226ec
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:40:58 +0200

    System Signature: 0c7ebe8de39a4b60791a9a11d7e3ba693de3a7137127cbb513410bdbe228eb6a

---
+++ control-panel/control-panel/blade.navbar.sidebar.php
@@ -0,0 +1,13 @@
+<div class="header"></div>
+
+<!-- 
+<div class="header">
+  <div onclick="change_page(`profile`)" class="user-settings">
+    <img class="user-img"
+            style="filter: contrast(0.2)"
+            src="trash/profile.png" alt="Profile" />
+    <div class="user-name">Black Sheep</div>
+  </div>
+</div>
+
+-->
