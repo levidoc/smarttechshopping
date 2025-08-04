cf77a1c30b834084e6735190fcc7276c0d26c2e8
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:38 +0200

    System Signature: e8075d3bc318ce7237ffd5100ee29045f98d5274c755ca4d5709a936726ed67e

---
+++ pages/user_security.php
@@ -0,0 +1,71 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('User Security', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Site Policies</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                User Security
+            </h3>
+        </div>
+
+        <div class="bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+            <p class="cl5 ">
+            <div id="collapse_MANDATORY_POLICY_5" style="padding: 1rem; display: block;">
+                Effective Date: 2024/03/06<br>
+                <br>
+                At Cross Gen, we prioritize the security of your financial transactions and strive to provide a seamless and trustworthy payment experience. This Payment and Security Policy outlines our commitment to protecting your payment information and the payment methods we accept. Please read this policy carefully to understand how we ensure the safety and confidentiality of your payment details.<br>
+                <br>
+                1. Secure Payment Methods:<br>
+                <br>
+                We offer a variety of secure payment methods to accommodate our customers' preferences. At present, we accept the following payment methods:<br>
+                <br>
+                a. Credit and Debit Cards: We accept major credit cards, including Visa, MasterCard, American Express, and Discover, as well as debit cards associated with these card networks.<br>
+                <br>
+                b. PayPal: Cross Gen is proud to offer PayPal as a secure payment option. PayPal allows you to make payments using your PayPal balance, linked bank account, or credit/debit card.<br>
+                <br>
+                c. Other Approved Payment Methods: In addition to credit cards and PayPal, we may offer other payment methods based on your location and our partnerships with trusted payment service providers.<br>
+                <br>
+                2. Payment Security:<br>
+                <br>
+                a. Encryption and Secure Socket Layer (SSL): Cross Gen uses industry-standard encryption protocols, such as SSL, to secure your payment information during transmission. SSL technology ensures that your data is encrypted and cannot be intercepted by unauthorized parties.<br>
+                <br>
+                b. Payment Card Industry Data Security Standard (PCI DSS): We comply with PCI DSS requirements to safeguard your credit card information. Your credit card details are neither stored nor visible to our staff. PCI DSS compliance ensures the highest level of security for your payment data.<br>
+                <br>
+                3. Fraud Prevention:<br>
+                <br>
+                a. Identity Verification: To prevent fraudulent transactions, we may perform identity verification checks on certain orders. In such cases, you may be asked to provide additional information or documentation to verify your identity.<br>
+                <br>
+                b. Suspicious Activity Monitoring: We monitor transactions for any suspicious or unauthorized activity and take necessary measures to prevent potential fraud.<br>
+                <br>
+                4. Third-Party Payment Processors:<br>
+                <br>
+                For certain payment methods, such as credit cards and PayPal, we may use third-party payment processors to facilitate transactions. These payment processors have their own privacy and security policies, and we encourage you to review them before proceeding with payment.<br>
+                <br>
+                5. Order Confirmation:<br>
+                <br>
+                After successfully placing an order, you will receive an order confirmation email that includes the details of your purchase. Please review this email and contact our customer support team (refer to the Customer Support and Contact Information Policy) if you notice any discrepancies.<br>
+                <br>
+                6. Policy Updates:<br>
+                <br>
+                This Payment and Security Policy may be updated from time to time to reflect changes in our payment processing and security practices. Any revisions will be communicated on our website, and the updated policy will be effective from the date of posting.<br>
+                <br>
+                If you have any questions or require further clarification regarding our Payment and Security Policy, please don't hesitate to contact us.<br>
+                <br>
+                Thank you for entrusting us with your payment information and for choosing Cross Gen!
+            </div>
+            </p>
+
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
