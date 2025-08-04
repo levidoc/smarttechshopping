f161a93dd5123643a025a832aadf17215415fceb
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:45:45 +0200

    System Signature: 430763723f7c9347d9dddeafa7a5bb2b1fe769a7b151a26da5a65b41a2f1ce49

---
+++ control-panel/website-builder/engines/interface.inspector.blade.php
@@ -0,0 +1,262 @@
+<div class="inspector" style="z-index: 10; height: 80vh !important; position: absolute; left: calc(100vw - 16rem); top: 90px;">
+                <input class="nav-inspector" name="nav-inspector" type="radio" id="design" checked="">
+                <input class="nav-input" name="nav-inspector" type="radio" id="prototype">
+                <input class="nav-inspector" name="nav-inspector" type="radio" id="code">
+                <input class="nav-inspector" name="nav-inspector" type="radio" id="right-arrow">
+                <div class="inspector-sections">
+                    <div class="wrapper">
+                        <label for="design" checked="">Design</label>
+                        <label for="prototype" checked="">Prototype</label>
+                        <label for="code" checked="">Code</label>
+                        <label for="right-arrow" checked="">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5.75 12.75L10.25 8.25L5.75 3.75" stroke="#3C3E52" stroke-width="2"></path>
+                            </svg>
+                        </label>
+                    </div>
+                </div>
+                <div class="inspector-tools">
+                    <div class="wrapper">
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="6" y1="20" x2="6" y2="4" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="9" y1="8" x2="20" y2="8" stroke="currentcolor" stroke-width="4"></line>
+                                <line x1="9" y1="14" x2="16" y2="14" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="12" y1="20" x2="12" y2="4" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="5" y1="8" x2="19" y2="8" stroke="currentcolor" stroke-width="4"></line>
+                                <line x1="7" y1="14" x2="17" y2="14" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(0 -1 -1 0 17 20)" stroke="currentcolor" stroke-width="2"></line>
+                                <line y1="-2" x2="11" y2="-2" transform="matrix(-1 0 0 1 15 10)" stroke="currentcolor" stroke-width="4"></line>
+                                <line y1="-2" x2="7" y2="-2" transform="matrix(-1 0 0 1 15 16)" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(-1 0 0 1 20 7)" stroke="currentcolor" stroke-width="2"></line>
+                                <line y1="-2" x2="11" y2="-2" transform="matrix(0 1 1 0 10 9)" stroke="currentcolor" stroke-width="4"></line>
+                                <line y1="-2" x2="7" y2="-2" transform="matrix(0 1 1 0 16 9)" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="20" y1="12" x2="4" y2="12" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="8" y1="19" x2="8" y2="5" stroke="currentcolor" stroke-width="4"></line>
+                                <line x1="14" y1="17" x2="14" y2="7" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="dark-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="20" y1="18" x2="4" y2="18" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="8" y1="15" x2="8" y2="4" stroke="currentcolor" stroke-width="4"></line>
+                                <line x1="14" y1="15" x2="14" y2="8" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="light-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(0 -1 -1 0 17 20)" stroke="currentcolor" stroke-width="2"></line>
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(0 -1 -1 0 5 20)" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="12" y1="15" x2="12" y2="9" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+                        </button>
+                        <button class="inspector-tool-item">
+                            <svg class="light-item" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(-1 0 0 1 20 7)" stroke="currentcolor" stroke-width="2"></line>
+                                <line y1="-1" x2="16" y2="-1" transform="matrix(-1 0 0 1 20 19)" stroke="currentcolor" stroke-width="2"></line>
+                                <line x1="15" y1="12" x2="9" y2="12" stroke="currentcolor" stroke-width="4"></line>
+                            </svg>
+
+                        </button>
+                    </div>
+                </div>
+                <div class="inspector-inputs">
+                    <div class="wrapper">
+                        <div class="left">
+                            <div class="input-wrapper">
+                                <p>X</p>
+                                <input type="text" placeholder="621">
+                            </div>
+                            <div class="input-wrapper">
+                                <p>Y</p>
+                                <input type="text" placeholder="412">
+                            </div>
+                            <div class="input-wrapper">
+                                <p>W</p>
+                                <input type="text" placeholder="98">
+                            </div>
+                            <div class="input-wrapper">
+                                <p>H</p>
+                                <input type="text" placeholder="98">
+                            </div>
+                            <div class="input-wrapper">
+                                <p>R</p>
+                                <input type="text" placeholder="0º">
+                            </div>
+                        </div>
+                        <div class="right">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 6V5C5 3.34315 6.34315 2 8 2C9.65685 2 11 3.34315 11 5V6" stroke="#8D8EA1" stroke-width="2"></path>
+                                <path d="M5 10V11C5 12.6569 6.34315 14 8 14C9.65685 14 11 12.6569 11 11V10" stroke="#8D8EA1" stroke-width="2"></path>
+                                <line x1="8" y1="12" x2="8" y2="4" stroke="#8D8EA1" stroke-width="2"></line>
+                            </svg>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="inspector-constraints">
+                    <div class="wrapper">
+                        <p class="inspector-header">CONSTRAINTS</p>
+                        <div class="constraints-wrapper">
+                            <div class="left-area">
+                                <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect x="0.5" y="0.5" width="65" height="65" rx="2.5" stroke="#4D4F60"></rect>
+                                    <rect x="20.5" y="20.5" width="25" height="25" rx="0.5" stroke="#4D4F60" stroke-miterlimit="16" stroke-linecap="square" stroke-dasharray="4 4"></rect>
+                                    <line x1="33" y1="61" x2="33" y2="51" stroke="#4D4F60" stroke-width="4"></line>
+                                    <line x1="33" y1="15" x2="33" y2="5" stroke="#3ED67E" stroke-width="4"></line>
+                                    <line x1="33" y1="39" x2="33" y2="27" stroke="#4D4F60" stroke-width="2"></line>
+                                    <line x1="5" y1="32" x2="15" y2="32" stroke="#3ED67E" stroke-width="4"></line>
+                                    <line x1="27" y1="33" x2="39" y2="33" stroke="#4D4F60" stroke-width="2"></line>
+                                    <line x1="51" y1="32" x2="61" y2="32" stroke="#4D4F60" stroke-width="4"></line>
+                                </svg>
+                            </div>
+                            <div class="right-area">
+                                <div class="first-constraint">
+                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="4" y1="8" x2="12" y2="8" stroke="#8D8EA1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
+                                        <path d="M2 8L4.5 5.5V10.5L2 8Z" fill="#8D8EA1"></path>
+                                        <path d="M13.5 8L11 5.5V10.5L13.5 8Z" fill="#8D8EA1"></path>
+                                    </svg>
+                                    <div class="styled-select">
+                                        <select class="styled-select">
+                                            <option>Left</option>
+                                            <option>Right</option>
+                                        </select>
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8 10L4.5359 6.25L11.4641 6.25L8 10Z" fill="#8D8EA1"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="second-constraint">
+                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="8" y1="12" x2="8" y2="4" stroke="#8D8EA1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
+                                        <path d="M8 14L5.5 11.5H10.5L8 14Z" fill="#8D8EA1"></path>
+                                        <path d="M8 2.5L5.5 5L10.5 5L8 2.5Z" fill="#8D8EA1"></path>
+                                    </svg>
+                                    <div class="styled-select">
+                                        <select class="styled-select">
+                                            <option>Top</option>
+                                            <option>Bottom</option>
+                                        </select>
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8 10L4.5359 6.25L11.4641 6.25L8 10Z" fill="#8D8EA1"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="inspector-layer">
+                    <div class="wrapper">
+                        <p class="inspector-header">LAYER</p>
+                        <div class="layer-wrapper">
+                            <div class="styled-select">
+                                <select class="styled-select">
+                                    <option>Pass Through</option>
+                                    <option></option>
+                                </select>
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8 10L4.5359 6.25L11.4641 6.25L8 10Z" fill="#8D8EA1"></path>
+                                </svg>
+                            </div>
+                            <input type="text" placeholder="100%">
+                            <svg width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 10C12.5715 10 16 6.25 16 5C16 3.75 12.5715 0 8 0C3.42847 0 0 3.75 0 5C0 6.25 3.42847 10 8 10ZM8 8C9.65674 8 11 6.65686 11 5C11 3.34314 9.65674 2 8 2C6.34326 2 5 3.34314 5 5C5 6.65686 6.34326 8 8 8Z" fill="#4D4F60"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="inspector-fill">
+                    <div class="wrapper">
+                        <div class="header">
+                            <p class="inspector-header">FILL</p>
+                            <div class="fill-icons">
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line x1="4" y1="8.53333" x2="12" y2="8.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                </svg>
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line x1="8" y1="12.5333" x2="8" y2="4.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                    <line x1="4" y1="8.53333" x2="12" y2="8.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                </svg>
+                            </div>
+                        </div>
+
+                        <div class="fill-content">
+                            <div class="color-input-area">
+                                <input type="text" placeholder="#3ED67E">
+                            </div>
+                            <input class="input-percent" type="text" placeholder="100%">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.5 9.27273C3.5 8.74889 3.76128 7.99741 4.22449 7.11006C4.67793 6.24145 5.28799 5.31032 5.90622 4.44883C6.52313 3.5892 7.14108 2.80868 7.60537 2.24253C7.75476 2.06036 7.88804 1.90063 8 1.76788C8.11196 1.90063 8.24524 2.06036 8.39463 2.24253C8.85892 2.80868 9.47687 3.5892 10.0938 4.44883C10.712 5.31032 11.3221 6.24145 11.7755 7.11006C12.2387 7.99741 12.5 8.74889 12.5 9.27273C12.5 11.5813 10.5121 13.5 8 13.5C5.48792 13.5 3.5 11.5813 3.5 9.27273Z" stroke="#4D4F60"></path>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M3.36328 11.0455C4.10498 9.31299 5.90137 8.09088 7.99976 8.09088C10.0981 8.09088 11.8945 9.31299 12.6362 11.0455C11.8945 12.7779 10.0981 14 7.99976 14C5.90137 14 4.10498 12.7779 3.36328 11.0455Z" fill="#4D4F60"></path>
+                            </svg>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 13C12.5715 13 16 9.25 16 8C16 6.75 12.5715 3 8 3C3.42847 3 0 6.75 0 8C0 9.25 3.42847 13 8 13ZM8 11C9.65674 11 11 9.65686 11 8C11 6.34314 9.65674 5 8 5C6.34326 5 5 6.34314 5 8C5 9.65686 6.34326 11 8 11Z" fill="#4D4F60"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="stroke">
+                    <div class="wrapper">
+                        <div class="header">
+                            <p class="inspector-header">STROKE</p>
+                            <div class="fill-icons">
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line x1="8" y1="12.5333" x2="8" y2="4.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                    <line x1="4" y1="8.53333" x2="12" y2="8.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="effects">
+                        <div class="wrapper">
+                            <div class="header">
+                                <p class="inspector-header">EFFECTS</p>
+                                <div class="fill-icons">
+                                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="8" y1="12.5333" x2="8" y2="4.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                        <line x1="4" y1="8.53333" x2="12" y2="8.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="export">
+                        <div class="wrapper">
+                            <div class="header">
+                                <p class="inspector-header">EXPORT</p>
+                                <div class="fill-icons">
+                                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="8" y1="12.5333" x2="8" y2="4.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                        <line x1="4" y1="8.53333" x2="12" y2="8.53333" stroke="#4D4F60" stroke-width="2"></line>
+                                    </svg>
+                                </div>
+                            </div>
+                            <p class="export-text">Click + to add an export setting</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
