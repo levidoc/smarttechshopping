9faa71fb98c43b5e9aa3886251f2945a01724908
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:57:58 +0200

    System Signature: e2aabeb97dd4078ba22be7d98145b0c23e9cb2d15b0af4406f00bee5fec42fa1

---
+++ pages/affiliate_program.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Affiliate Program', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Earn A Commision</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Affiliate Program
+            </h3>
+        </div>
+        <div class="flex-w flex-tr">
+            <div class="size-210  p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <img src="images/affiliate_marketing.gif" style="width:100%;">
+            </div>
+
+            <div class="size-210  flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <p class="cl5 ">This section has not been completed yet. Stay tunned to be notified when the affiliate program will be launched</p>
+                
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
