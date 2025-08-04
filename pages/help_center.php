c538aa4b19eda8120bd723f38de1cb0766c41175
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:13 +0200

    System Signature: bc87afc1f52d1ef5b4f36f2900907c2e793c068251debdfdea0704915729c570

---
+++ pages/help_center.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Help Center', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Get Help</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Support Page
+            </h3>
+        </div>
+        <div class="flex-w flex-tr">
+            <div class="size-210  p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <img src="images/help_center.gif" style="width:100%;">
+            </div>
+
+            <div class="size-210  flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <h3 class="ltext-106 cl5 txt-center">
+                    Support Contacts
+                </h3>
+                <br>
+                <p class="cl5">Our Support Team is here to help you. Feel free to contact the team.</p>
+                <br>
+                <p class="cl5"><i class="fa-solid fa-envelope"></i> support@varsitymarket.tech </p>
+                <p class="cl5"><i class="fa-solid fa-envelope"></i> billing@varsitymarket.tech (Account & Support)</p>
+                
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
