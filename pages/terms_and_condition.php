c07723fecad96f308bd3bfb219f636472566404b
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:35 +0200

    System Signature: 049173efe1dff34128ab871617be47a4c7b65c248d4fe567119f875f7d3c6c23

---
+++ pages/terms_and_condition.php
@@ -0,0 +1,113 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Terms And Conditions', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Site Policies</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Terms And Conditions
+            </h3>
+        </div>
+
+        <div class="bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+            <p class="cl5 ">
+                <br><br><span style="font-size: large;">Introduction</span>
+                <br>
+                <strong>Overview of Terms and Conditions:</strong><br><br>
+
+                Terms and conditions create a legally binding agreement between the store and its users. It sets out the rules and responsibilities for both parties, reducing the risk of legal disputes and providing legal protection for the store. The terms and conditions outline the terms of sale, including pricing, payment methods, shipping, delivery, returns, and refunds. This clarity helps prevent misunderstandings and customer complaints related to the purchasing process.
+                Terms and conditions can include disclaimers and liability limitations, protecting the store from certain legal claims or damages that may arise from the use of the website or the products sold. We often display copyrighted material, such as product images, descriptions, and branding. Terms and conditions can specify how users are allowed to use this content and protect the store's intellectual property rights.
+                With the collection of personal data during the purchasing process, terms and conditions can inform users about how their data will be used, stored, and protected, ensuring compliance with privacy laws and building trust with customers. Terms and conditions can outline prohibited activities in the store, such as fraud, hacking attempts, or misuse of the website. This helps maintain a safe and secure environment for both the store and its customers.
+                Our Terms and conditions may include communication policies, specifying how the eCommerce store and customers will interact, including email communication, marketing messages, and customer support. We do set out the responsibilities of users, such as providing accurate information during the checkout process or keeping login credentials secure, ensuring a smooth and secure shopping experience.
+                Our Terms and conditions usually contain provisions about how and when the terms may be updated or changed. This allows the eCommerce store to adapt to changing circumstances and ensure that users are aware of any modifications.
+
+                <br><br><strong>Why Users Should Adhere to The Terms and Conditions:</strong><br>
+
+                Terms and conditions form a legally binding contract between users and Cross Gen Admins. By using our platform, users agree to abide by the specified rules and conditions. Failure to adhere to these terms may lead to legal consequences or contractual disputes. Our Terms and Conditions often include clauses that protect the intellectual property rights of service providers, such as copyrights, trademarks, and patents. Users need to respect these rights to avoid infringing on copyrighted material or proprietary information.
+                Our terms and conditions do outline the privacy practices of our platform, explaining how user data is collected, stored, and used. Adhering to these terms helps protect users' personal information and ensures data security and confidentiality. Our Terms and conditions often include guidelines for acceptable behavior and usage. Adhering to these rules helps maintain a safe and respectful environment for all users and prevents abusive or harmful actions.
+                Some services may have limitations or access restrictions based on user compliance with terms and conditions. By adhering to these rules, users ensure they can continue to access and use all the features provided. Violating terms and conditions can result in account suspension or termination. This can lead to loss of access to essential services and any associated data or content. Users who adhere to terms and conditions demonstrate respect for the Cross Gen platform and other users. This helps build trust and credibility within the community and may positively impact their reputation.
+                Adhering to terms and conditions promotes fairness among users as everyone is expected to follow the same set of rules and guidelines. Terms and conditions are often designed to ensure compliance with relevant laws and regulations. By following these terms, users also comply with legal requirements and avoid potential legal issues. User feedback and behavior can be valuable for service providers to improve their offerings. By adhering to terms and conditions, users contribute to the platform’s ongoing development and enhancement.
+                
+                <br><br><strong>Modifications to Terms and Conditions:</strong><br>
+
+                When modifications are made to our store's Terms and Conditions or any other policy. We would like to send notifications or other forms of communication to users informing them of the changes that have occurred. Signed Users of the store would be notified of the changes via email. The changes would be visible to users in the help section.
+                Notifying users of any changes in the terms and conditions demonstrates transparency in our store. Store admins appreciate knowing that their users are aware when there have been updates to the rules and guidelines governing their interactions with the platform. This transparency builds trust between the store and its customers.
+                When users are made aware of changes to the terms and conditions, they have the opportunity to review the modifications and make an informed decision about whether they still want to continue using the platform. It ensures that users are not caught unaware by unexpected alterations that could affect their rights or obligations.
+                In some jurisdictions, there may be legal requirements for notifying users of changes to terms and conditions. By providing clear and conspicuous notices, the store can adhere to these legal obligations and avoid potential legal issues.
+                Notifying users of changes in terms and conditions allows them to take necessary precautions or adjustments to their behavior if the changes impact their rights, privacy, or data protection. Users can also make more informed choices about whether they wish to continue using the platform based on the updated terms.
+                Without proper notice of changes, users may inadvertently violate the updated terms and conditions, leading to disputes or misunderstandings between users and the store. Clear warnings help minimize such occurrences. By proactively informing users about changes, the store demonstrates respect for its customers and their rights. This positive communication approach can lead to better customer retention and satisfaction.
+                Providing clear warnings of changes in terms and conditions helps the store establish that users have been properly notified in case of any future disputes or disagreements. The notice of changes can serve as an educational opportunity for users to understand the reasons behind the modifications and the potential impact on their relationship with the store. Regularly warning users about changes to terms and conditions showcases the store's professionalism and commitment to keeping its user base well-informed.
+
+                <br><br><strong> Definitions</strong><br><br>
+
+                Please note that within the description of this policy, we would use different keywords to describe Cross Gen’s store, such as our, us, admins, store, platform, business and organization.
+
+                <br><br><strong>Use of Website and Services</strong><br>
+
+                By accessing and using this store website and its services, you agree to be bound by these terms and conditions. If you do not agree to these terms, you should not use the website.
+                You must be of legal age in your jurisdiction to use this website. By using the website, you confirm that you meet this eligibility requirement.
+                Some features and services may require account registration. When creating an account, you agree to provide accurate and complete information. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.
+                <br><br>You agree not to engage in any of the following prohibited activities:<br>
+                <br>- Violating any applicable laws or regulations.
+                <br>- Using the website for any illegal or unauthorized purpose.
+                <br>- Interfering with the website's functionality, security, or accessibility.
+                <br>- Engaging in fraudulent or deceptive practices.
+                <br>- Uploading or distributing any malicious software or content.
+                <br>- Harassing, threatening, or abusing other users or website staff.<br>
+                The website strives to provide accurate product information and pricing. However, we do not guarantee the accuracy of such information. Prices and availability are subject to change without notice. All payments made on the website are securely processed. By using the website, you agree to the website's payment processing policies.
+                Your privacy is important to us. By using the website, you consent to the collection, use, and storage of your personal information in accordance with our Privacy Policy.
+                If you submit any user-generated content on the website (e.g., reviews, comments, etc.), you grant the website owner a non-exclusive, royalty-free, perpetual, and worldwide license to use, modify, and distribute that content.
+                The website reserves the right to terminate or suspend your access to the website and services at any time, without prior notice, for any violation of these terms or for any other reason. The website may update these terms and conditions from time to time. Any changes will be effective upon posting on the website. By continuing to use the website after the changes, you accept the updated terms.
+                Please read these terms and conditions carefully before using the website. If you have any questions or concerns, please contact the store admin.
+
+                <br><br><strong>Privacy Policy</strong><br>
+
+                For convenience and to prevent redundancy, the privacy policy was subsequently omitted from the terms and conditions. This would imply that if users want to know how the website collects and maintains personal data, they should read our statement of privacy.
+
+                <br><br><strong>Merchandise Information and Pricing</strong><br>
+
+                The purpose of this policy is to establish guidelines and procedures for making changes to product information and pricing in the store. This policy aims to ensure accuracy, consistency, and transparency in communicating product details and prices to our customers, suppliers, and internal stakeholders.
+                This policy applies to all employees, managers, and stakeholders involved in the process of updating, maintaining, and communicating product information and pricing in the store.
+                The Product Management team is responsible for overseeing and managing all product-related information. They shall collaborate with relevant departments to ensure that product details, specifications, descriptions, and images are accurate and up to date.
+                Any changes to product information are documented and submitted to the Product Management team for review and approval. The team will assess the proposed changes for accuracy, compliance, and alignment with the store's overall product strategy.
+                Once the product information changes are approved, the Product Management team will communicate the updates to all relevant departments, including Sales, Marketing, Customer Support, and Website Management. This communication should be timely and comprehensive to ensure consistency across all channels.
+                A Pricing Committee shall be established, consisting of representatives from Finance, Sales, Marketing, and Product Management. The committee will be responsible for reviewing and approving any changes to product pricing.
+                <br><br><strong>Factors Considered for Pricing Changes</strong><br>
+                The Pricing Committee shall consider various factors before approving pricing changes, including but not limited to:
+                <br> Cost of production
+                <br> Market demand and competition
+                <br> Currency fluctuations (for international sales)
+                <br> Seasonal or promotional pricing strategies
+                <br> Profit margins and financial goals
+
+                <br><br><strong>The Pricing Committee will follow a structured process for pricing changes:</strong><br>
+                <br> Initiation: A pricing change proposal is submitted to the Pricing Committee by the Product Management team.
+                <br> Analysis: The committee evaluates the proposal based on the factors mentioned above
+                <br> Approval: If the proposal aligns with the company's pricing strategy and financial objectives, it is approved by the Pricing Committee.
+                <br> Communication: The approved pricing changes are communicated to the Sales team, Website Management, and other relevant stakeholders for implementation.
+                <br><br>
+                Special offers, discounts, and promotions shall be managed by the Marketing and Sales teams in alignment with the pricing strategy and overall marketing plan. The Pricing Committee may be involved in evaluating the potential impact of significant promotions on the company's financials.
+                All product information and pricing changes must comply with local laws and regulations, as well as any industry-specific standards or guidelines.
+                This policy shall be regularly reviewed and updated by the Product Management team in collaboration with relevant stakeholders to ensure its effectiveness and relevance.
+                The accessible state of particular merchandise will be mentioned. If particular products have been cited in your invoice order but are no longer available. A complete refund will be issued, and the user will be apprised of the status of the merchandise. It is beneficial for the user if they are given with defective products to provide visual evidence of the product (images/videos) and suggest that the admins investigate why the product was damaged as well as establish a strategy to reimburse users.
+                Shipping and Delivery
+                At Cross Gen, we strive to deliver your purchases quickly and efficiently. This Shipping and Delivery Policy outlines our procedures for shipping, estimated delivery times, and related information. Please read our shipping policy carefully to understand our commitment to providing a smooth and reliable shipping experience.
+
+
+
+
+            </p>
+
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
