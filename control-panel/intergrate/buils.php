b80f55da09efb635be7619facaec7c9fc8f83f2c
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:44:05 +0200

    System Signature: f99ab18043a5572d6fdfcfb69cc215f3276d74f2bfdeb4659fd63abb804ef37e

---
+++ control-panel/intergrate/buils.php
@@ -0,0 +1,8 @@
+<?php
+include_once "header.php";
+include_once "navbar.php";
+include_once "profile.blade.php";
+include_once "scriots.blade.php";
+include_once "page.build.php";
+include_once "footer.php";
+?>
