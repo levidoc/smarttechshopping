3abb0635c7cc5af5d930a36249bf7b2b433e9801
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:44:08 +0200

    System Signature: e804770c199f05403e70c7bec05679527ca076601adddfad0935aee6d3064c8d

---
+++ control-panel/intergrate/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!-- saved from url=(0049)https://beta-v1.varsitymarket.shop/online-stores/ -->
+<html
+  :class="{ &#39;theme-dark&#39;: dark }"
+  x-data="data()"
+  lang="en"
+  class="theme-dark"
+>
+  <head trust-key="">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <!-- Google tag (gtag.js) -->
+    <script async="" src="./Store Products_files/js"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-YVH6L2RLM1");
+    </script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Store Products</title>
+    <link href="./Store Products_files/css2" rel="stylesheet" />
+    <link rel="stylesheet" href="./Store Products_files/animate.min.css" />
+    <!-- <base href="http://varsitymarket.store/"> -->
+    <style></style>
+    <!--<base href="https://beta-v1.varsitymarket.shop/">-->
+    <base href="." />
+
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="https://beta-v1.varsitymarket.shop/assets/img/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="https://beta-v1.varsitymarket.shop/assets/img/favicon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="https://beta-v1.varsitymarket.shop/assets/img/favicon.png"
+    />
+    <link
+      rel="manifest"
+      href="https://beta-v1.varsitymarket.shop/assets/img/site.webmanifest"
+    />
+
+    <link rel="stylesheet" href="./Store Products_files/tailwind.output.css" />
+    <link rel="stylesheet" href="./Store Products_files/cleanup.css" />
+    <link rel="stylesheet" href="./Store Products_files/style.css" />
+    <script
+      src="./Store Products_files/alpine.min.js.download"
+      defer=""
+    ></script>
+
+    <script src="./Store Products_files/init-alpine.js.download"></script>
+    <script src="./Store Products_files/inbox_notification.js.download"></script>
+    <link rel="stylesheet" href="./Store Products_files/Chart.min.css" />
+
+    <link rel="stylesheet" href="./Store Products_files/all.min.css" />
+    <script src="./Store Products_files/chart.min.js.download"></script>
+  </head>
