271c4a36b93ce8d3794bea64b9fe500b2434bf24
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:59:32 +0200

    System Signature: 759e72ca096dbf5c2e66d2bdc9e5f834790270b059432b402c069697d0ab7267

---
+++ plugin/yoco_temp_keys.php
@@ -0,0 +1,10 @@
+<?php 
+DEFINE('YOCO_TEST_SECRET_KEY','sk_test_c5391d7elVD83oy19b14a02bc44e');
+DEFINE('YOCO_TEST_PUBLIC_KEY','pk_test_27cc3eaevnLgBm8c4ec4'); 
+//the testing keys 
+DEFINE('YOCO_LIVE_SECRET_KEY','sk_live_44955437lVD83oybb484cb9820f3');
+DEFINE('YOCO_LIVE_PUBLIC_KEY','pk_live_dca6fdcevnLgBm8864d4'); 
+//The deployment keys 
+DEFINE('YOCO_ACTION','TEST'); 
+//Define Wheather the application should go from testing to deployment
+?>
