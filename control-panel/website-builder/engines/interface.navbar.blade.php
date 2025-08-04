f9cc00d1ec03a78d277da139fefd7fa6e08228fb
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:45:47 +0200

    System Signature: 8d9c787300f6c3bc099ddb3ef187474c236e6c8d66e15b7a9a7a88e6e023f987

---
+++ control-panel/website-builder/engines/interface.navbar.blade.php
@@ -0,0 +1,226 @@
+<div class="side-bar">
+    <ul class="collapsible-accordion">
+        <li>
+            <div class="collapsible-header">
+                <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                    </svg>
+                </span>
+                <p><i class="fa-solid fa-file"></i> Home Page</p>
+            </div>
+            <div class="collapsible-body">
+                <ul class="collapsible-content">
+                    <li>
+                        <div class="collapsible-sub-header">
+                            <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                                </svg>
+                            </span>
+                            <span class="folder">
+                                <svg width="16" height="13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2h15a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2z" fill="url(#paint0_linear)"></path>
+                                    <path d="M0 1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1H0V1z" fill="url(#paint1_linear)"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="1" y1="3.5" x2="14.157" y2="6.891" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                        <linearGradient id="paint1_linear" x1=".875" y1=".273" x2="5.711" y2="6.272" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </span>
+                            <span>Header</span>
+                        </div>
+                        <div class="collapsible-body"></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-sub-header">
+                            <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                                </svg>
+                            </span>
+                            <span class="folder">
+                                <svg width="16" height="13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2h15a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2z" fill="url(#paint0_linear)"></path>
+                                    <path d="M0 1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1H0V1z" fill="url(#paint1_linear)"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="1" y1="3.5" x2="14.157" y2="6.891" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                        <linearGradient id="paint1_linear" x1=".875" y1=".273" x2="5.711" y2="6.272" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </span>
+                            <span>Sub header</span>
+                        </div>
+                        <div class="collapsible-body">
+                            <ul class="collapsible-sub-content">
+                                <li>
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <line x1="6" y1="9" x2="6" y2="7" stroke="#4D4F60" stroke-width="2"></line>
+                                            <line x1="18" y1="9" x2="18" y2="7" stroke="#4D4F60" stroke-width="2"></line>
+                                            <line x1="12" y1="18" x2="12" y2="8" stroke="#4D4F60" stroke-width="2"></line>
+                                            <line x1="5" y1="6" x2="19" y2="6" stroke="#4D4F60" stroke-width="2"></line>
+                                            <line x1="9" y1="19" x2="15" y2="19" stroke="#4D4F60" stroke-width="2"></line>
+                                        </svg>
+                                    </span>
+                                    <span>Title</span>
+                                </li>
+                                <li class="content-wrapper">
+                                    <span class="list-content-left">
+                                        <span>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <line x1="6" y1="9" x2="6" y2="7" stroke="#4D4F60" stroke-width="2"></line>
+                                                <line x1="18" y1="9" x2="18" y2="7" stroke="#4D4F60" stroke-width="2"></line>
+                                                <line x1="12" y1="18" x2="12" y2="8" stroke="#4D4F60" stroke-width="2"></line>
+                                                <line x1="5" y1="6" x2="19" y2="6" stroke="#4D4F60" stroke-width="2"></line>
+                                                <line x1="9" y1="19" x2="15" y2="19" stroke="#4D4F60" stroke-width="2"></line>
+                                            </svg>
+                                        </span>
+                                        <span>Sub title</span>
+                                    </span>
+                                    <span class="list-content-right">
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3 7H13V14H3V7Z" fill="#515367"></path>
+                                            <path d="M5 7V4C5 2.89543 6.34315 2 8 2C9.65685 2 11 2.89543 11 4V7" stroke="#515367" stroke-width="2"></path>
+                                        </svg>
+                                    </span>
+                                </li>
+                                <li class="content-wrapper">
+                                    <span class="list-content-left">
+                                        <span>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <line x1="5.29289" y1="18.2929" x2="18.0208" y2="5.56497" stroke="#D5D7E8" stroke-width="2"></line>
+                                            </svg>
+                                        </span>
+                                        <span>Line</span>
+                                    </span>
+                                    <span class="list-content-right">
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 13C12.5714 13 16 9.25 16 8C16 6.75 12.5714 3 8 3C3.42857 3 0 6.75 0 8C0 9.25 3.42857 13 8 13ZM8 11C9.65686 11 11 9.65686 11 8C11 6.34314 9.65686 5 8 5C6.34314 5 5 6.34314 5 8C5 9.65686 6.34314 11 8 11Z" fill="#515367"></path>
+                                        </svg>
+                                    </span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="3.99994" y="12" width="11.3137" height="11.3137" transform="rotate(-45 3.99994 12)" stroke="#BB6BD9" stroke-width="2"></rect>
+                                        </svg>
+                                    </span>
+                                    <span class="font-color">Elements/Logo</span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="8.99997" y="7" width="4.24264" height="4.24264" transform="rotate(-45 8.99997 7)" fill="#713588" stroke="#BB6BD9" stroke-width="2"></rect>
+                                            <rect x="3.99997" y="12" width="4.24264" height="4.24264" transform="rotate(-45 3.99997 12)" fill="#713588" stroke="#BB6BD9" stroke-width="2"></rect>
+                                            <rect x="14" y="12" width="4.24264" height="4.24264" transform="rotate(-45 14 12)" fill="#713588" stroke="#BB6BD9" stroke-width="2"></rect>
+                                            <rect x="8.99997" y="17" width="4.24264" height="4.24264" transform="rotate(-45 8.99997 17)" fill="#713588" stroke="#BB6BD9" stroke-width="2"></rect>
+                                        </svg>
+                                    </span>
+                                    <span class="font-color">Buttons - CTA</span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="5" y="5" width="14" height="14" stroke="#4D4F60" stroke-width="2"></rect>
+                                        </svg>
+                                    </span>
+                                    <span>Background</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="collapsible-sub-header">
+                            <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                                </svg>
+                            </span>
+                            <span class="folder">
+                                <svg width="16" height="13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2h15a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2z" fill="url(#paint0_linear)"></path>
+                                    <path d="M0 1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1H0V1z" fill="url(#paint1_linear)"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="1" y1="3.5" x2="14.157" y2="6.891" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                        <linearGradient id="paint1_linear" x1=".875" y1=".273" x2="5.711" y2="6.272" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </span>
+                            <span>Content</span>
+                        </div>
+                        <div class="collapsible-body"></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-sub-header">
+                            <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                                </svg>
+                            </span>
+                            <span class="folder">
+                                <svg width="16" height="13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2h15a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2z" fill="url(#paint0_linear)"></path>
+                                    <path d="M0 1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1H0V1z" fill="url(#paint1_linear)"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="1" y1="3.5" x2="14.157" y2="6.891" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                        <linearGradient id="paint1_linear" x1=".875" y1=".273" x2="5.711" y2="6.272" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#5492FF"></stop>
+                                            <stop offset="1" stop-color="#545EFF"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </span>
+                            <span>Footer</span>
+                        </div>
+                        <div class="collapsible-body"></div>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header">
+                <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                    </svg>
+                </span>
+                <p>2.Logo</p>
+            </div>
+            <div class="collapsible-body">
+                <ul class="collapsible-content">
+                </ul>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header">
+                <span class="arrow"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.5 6L8 10.5L12.5 6" stroke="#515367" stroke-width="2"></path>
+                    </svg>
+                </span>
+                <p>3.Icons</p>
+            </div>
+            <div class="collapsible-body">
+                <ul class="collapsible-content">
+                </ul>
+            </div>
+        </li>
+
+    </ul>
+
+</div>
