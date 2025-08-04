5cfb8b66e822631017c7432017afa4d0b3298162
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:41:05 +0200

    System Signature: dd19d7ce8005a3a33b49f00c4f9e88e8ce1ab46438bcabc4b8316a0d874321d2

---
+++ control-panel/control-panel/config.php
@@ -0,0 +1,22 @@
+<?php 
+if (!defined("__EMAIL_PASSWORD__")){
+    define("__EMAIL_PASSWORD__","the password here"); 
+}
+
+if (!defined("__EMAIL_PORT__")){
+    define("__EMAIL_PORT__","587"); 
+}
+
+if (!defined("__EMAIL_USERNAME__")){
+    define("__EMAIL_USERNAME__","Hastings"); 
+}
+
+if (!defined("__EMAIL_HOST__")){
+    define("__EMAIL_HOST__","smtp.gmail.com"); 
+}
+
+if (!defined("__EMAIL_ADDRESS__")){
+    define("__EMAIL_ADDRESS__","hardyhastings@gmail.com"); 
+}
+
+?>
