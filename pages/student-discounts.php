acd755f94e525d880f21bff0e8f9b3fb9f366164
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:33 +0200

    System Signature: 8adcdeb87ef2732c3acda29e7ab2a2ad61b841af59ff5e82773560391bd9cf14

---
+++ pages/student-discounts.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Track Your Order', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Store Discounts & Promo</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Student Discounts
+            </h3>
+        </div>
+        <div class="flex-w flex-tr">
+            <div class="size-210  p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <img src="images/new_idea.gif" style="width:100%;">
+            </div>
+
+            <div class="size-210  flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <div>
+                    <p class="cl5 txt-center m-b-20">Unfortunately, No discounts have been made available for students and college students.</p>
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
