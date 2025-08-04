3002003cec7c9b44eb2ad1fe355f10b19f554eb2
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:22 +0200

    System Signature: c3110454a5a36d1633d2ca552a0fd11eebe853f706b61cf8ac2e91cafa32e61d

---
+++ pages/privacy_policy.php
@@ -0,0 +1,68 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Privacy Policy', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Site Policies</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Privacy Policy
+            </h3>
+        </div>
+
+        <div class="bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+            <p class="cl5 ">
+                Privacy Policy for Marketplace<br>
+                <strong>Effective date: 2024/02/14</strong><br>
+                <br>Welcome to our Marketplace ! This Privacy Policy describes how we collect, use, disclose, and protect your personal information when you visit our website (the "Site") and use our services (the "Services"). By using varsitymarket , you agree to the practices described in this Privacy Policy.
+                <br><br><span style="font-size: large;">Information We Collect</span><br>
+                <strong>Personal Information:</strong> When you create an account or place an order on varsitymarket , we may collect certain personal information such as your name, email address, shipping address, billing address, and payment details. We may also collect additional information you provide voluntarily, such as your phone number or date of birth. Please note that as an attempt to increase your privacy we decided to enable more encryption algorithms that process your personal information and the information you submit to us. All these go through a double phase encryption layer and use end-to-end encryption to make sure that data is kept secure through transfer.<br>
+                <strong>Browsing Information:</strong> We collect non-personal information automatically when you access the Site. This information may include your IP address, browser type, device type, operating system, referring URLs, and pages you view on our Site.<br>
+                <strong>Cookies and Similar Technologies:</strong> We use cookies and similar technologies to enhance your experience on our Site. These technologies help us understand how you use our Site, personalize your experience, and track your interactions with our Services. You can manage your cookie preferences through your browser settings.<br>
+
+                <br><br><span style="font-size: large;">How We Use Your Information</span><br>
+                <b>Order Fulfillment:</b> We use your personal information to process and fulfill your orders, including shipping and payment processing.<br>
+                <b>Customer Support:</b> We may use your information to provide customer support and respond to your inquiries or requests.<br>
+                <b>Personalization:</b> Your information allows us to personalize your shopping experience and show you relevant products, promotions, and content.<br>
+                <b>Marketing Communications:</b> With your consent, we may send you marketing emails or notifications about new products, discounts, or special offers. You can opt-out of marketing communications at any time.<br>
+                <b>Improvement of Services:</b> We may use aggregated and anonymized data to analyze and improve our Services, website functionality, and user experience.<br>
+
+                <br><br><span style="font-size: large;">How We Share Your Information</span><br>
+                <b>Third-Party Service Providers:</b> We may share your information with third-party service providers who assist us in operating our business, such as shipping carriers, payment processors, and IT service providers.<br>
+                <b>Legal Compliance:</b> We may disclose your information to comply with applicable laws, regulations, or legal processes, or to respond to a valid government request.<br>
+
+                <br><br><span style="font-size: large;">Your Choices</span><br>
+                <br><b>Account Information:</b> You can review and update your account information by logging into your vm account.
+
+                <br><b>Marketing Communications:</b> You can opt-out of receiving marketing emails by clicking the "unsubscribe" link at the bottom of our emails.
+
+                <br><b>Cookies:</b> Most web browsers are set to accept cookies by default. You can modify your browser settings to block cookies or alert you when cookies are being sent.
+
+                <br><br><span style="font-size: large;">Data Security</span><br>
+                We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.
+
+                <br><br><span style="font-size: large;">Cookie Policy And Other Storage Devices:</span><br>
+                To be fully envoloped in the general experience of beign a varsitymarket user, there are certain account preferences and settings that are stored within your device. These settings are stored as cookies and other device medias such as sessions and device logs. Please note that these cookies are used as a tracking mechanism to distinguish users apart. Incusive to distinguishing users, our cookies store account settings. All the information in the cookies go through a end-to-end encryption state to ensure that we increase our system security, this entails that all the information stored will always be encrypted on both website as well as device.
+
+                <br><br><span style="font-size: large;">Children's Privacy</span><br>
+                varsitymarket is not intended for use by individuals under the age of 16. We do not knowingly collect personal information from children under 16. If you are a parent or guardian and believe your child has provided us with personal information, please contact us immediately.
+                <br><br>
+                Changes to this Privacy Policy<br>
+                We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any modifications will be effective immediately upon posting the updated Privacy Policy on our Site.
+
+                Contact Us
+                If you have any questions, concerns, or suggestions regarding this Privacy Policy or our privacy practices, please contact our support team.
+            </p>
+
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
