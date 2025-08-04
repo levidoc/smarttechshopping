15d25333a93ca181ca00a80664745a1c71eec5f7
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:18 +0200

    System Signature: ac14a38f11eff4fd6ddc9618d9739a8cf343608bbeef7be1d30335d7cfebe6fb

---
+++ pages/order_process.php
@@ -0,0 +1,166 @@
+<?php
+include_once "function.php";
+
+$id = get_url_data('status');
+$status_info = strtoupper(simple_decryption($id)); 
+
+$retrieve_data = read_session('CHECKOUT'); 
+$vendor_code = read_session('VENDOR_CHECKOUT'); 
+
+if (($vendor_code == FALSE) && ($retrieve_data == FALSE)){
+    header('Location: ./account_order_history.php');
+    exit;
+}
+
+$order = json_decode($retrieve_data,true); 
+
+$order_data = api_retrieve_order_details($vendor_code,$order['ORDER_NUM']); 
+
+if (isset($order_data)){
+
+    $payment_build_info = ""; 
+    $order_currency = $order_data['CURRENCY']; 
+    $payment_build = $order_data['PAYMENT_BUILD']; 
+    foreach ($payment_build as $description=>$amount){
+        $payment_build_info .= '
+        <div class="flex-w flex-t bor12 p-b-13" style="align-items: center">
+            <div class="size-208">
+                <span class="stext-110 cl2">
+                    '.$description.'
+                </span>
+            </div>
+
+            <div class="size-209" style="text-align: end; padding: 1rem;">
+                <span class="mtext-110 cl2">
+                '.retrieve_currency_code('CURRENCY_SIGN').' '.number_format(currency_convert($amount,$order_currency),2,'.',' ').'
+                </span>
+            </div>
+        </div>'; 
+    } 
+    
+    
+    $product_build_info = ""; 
+    $product_build = $order_data['PRODUCT_BUILD'];
+    foreach($product_build as $row){
+        $product_id = $row['INDEX']; 
+        $product_name = $row['META_DATA'][0]['TITLE']; 
+        $product_image = file_path('product_image').$row['META_DATA'][0]['COVER_IMG']; 
+        $product_quantity = $row['QUANTITY'];
+        $product_amount = currency_convert($row['AMOUNT'],$order_currency);  
+        remove_cart($product_id); 
+        $sum = $product_quantity * $product_amount; 
+
+        $product_size = $row['SIZE']; 
+        $product_color = $row['COLOR']; 
+                $x = '
+            <tr class="table_row">
+                <td class="column-1">
+                    <div class="how-itemcart1">
+                        <img src="'.$product_image.'" alt="'.$product_name.'">
+                    </div>
+                </td>
+                <td class="column-2">'.$product_name.'</td>
+                <td class="column-3">'.retrieve_currency_code('CURRENCY_SIGN').' '.number_format(ceil($product_amount),2,'.',' ').'</td>
+                <td class="column-4">x '.$product_quantity.'</td>
+                <td class="column-5">'.retrieve_currency_code('CURRENCY_SIGN').'  '.number_format(ceil($sum),2,'.',' ').'</td>
+            </tr>';
+        $product_build_info .= $x; 
+    }
+
+    $html_structure = '
+        <section class="bg0 p-t-104 p-b-116">
+            <div class="p-b-45">
+                <p class="cl5 txt-center">Order '.ucwords(strtolower($status_info)).'</p>
+                <h3 class="ltext-106 cl5 txt-center">
+                    Order #'.$order['ORDER_NUM'].'
+                </h3>
+            </div>
+
+            <form>
+                <div class="container">
+                    <div class="row" id="cart_container">
+
+
+                        <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
+                            <div class="m-l-25 m-r--38 m-lr-0-xl">
+                                <div class="wrap-table-shopping-cart">
+                                    <table class="table-shopping-cart">
+                                        <tbody>
+                                            <tr class="table_head">
+                                                <th class="column-1">Product</th>
+                                                <th class="column-2"></th>
+                                                <th class="column-3">Price</th>
+                                                <th class="column-4">Quantity</th>
+                                                <th class="column-5">Total</th>
+                                            </tr>
+
+                                            '.$product_build_info.'
+
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
+                            <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
+                                <h4 class="mtext-109 cl2 p-b-30">
+                                    Order Totals
+                                </h4>
+
+                                '.$payment_build_info.'
+
+                                <div class="flex-w flex-t p-t-27 p-b-33">
+                                    <div class="size-208">
+                                        <span class="mtext-101 cl2">
+                                            Total:
+                                        </span>
+                                    </div>
+
+                                    <div class="size-209 p-t-1" style="text-align: end; padding: 1rem;">
+                                        <span class="mtext-110 cl2">
+                                        '.retrieve_currency_code('CURRENCY_SIGN').number_format(ceil(currency_convert($order_data['AMOUNT'],$order_currency)),2,'.',' ').'
+                                        </span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </section>
+    '; 
+
+    $status_info = strtolower($status_info); 
+
+    $payment_method = "YOCO Payment Gateway"; 
+    $order_id = $order['ORDER_NUM']; 
+    if ($status_info == "cancel"){
+        $order_status = "cancelled"; 
+    }else if ($status_info == "success"){
+        $order_status = "completed"; 
+    }else {
+        $order_status = "failed"; 
+    }
+
+    $x = api_update_order($payment_method,$order_status,$order_id); 
+    if ($x == "UPDATED"){
+        clear_session(); 
+    }
+
+}else{
+    header('Location: ./index.php'); 
+    exit; 
+}
+
+
+include_once "top.php";
+echo (create_seo_signature('Cart', false, 'CROSS GEN', ''));
+
+?>
+<?php include_once "header.php" ?>
+<?php echo($html_structure); ?>
+<?php include_once "footer.php" ?>
