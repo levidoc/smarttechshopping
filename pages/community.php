643409dff488a42a6c69441fbaa0d5a36bd18183
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:07 +0200

    System Signature: e1bad7becea6fd2a5050b9866ec057a78e9c154442853ceaabad01bedfa9bcfb

---
+++ pages/community.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Community', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Converse with other users</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Community Platform
+            </h3>
+        </div>
+        <div class="flex-w flex-tr">
+            <div class="size-210  p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <img src="images/chatbox.gif" style="width:100%;">
+            </div>
+
+            <div class="size-210  flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <p class="cl5 ">This section has not been completed yet. Stay tunned to be notified when the community program will be launched</p>
+                <br>
+                <p class="cl5">Making communication with our users easy and more convinient </p> 
+
+                
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
