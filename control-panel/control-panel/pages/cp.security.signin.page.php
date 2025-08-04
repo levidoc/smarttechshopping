e1a0ee653229e6885e09c0a8b174e550eb9e1552
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:41:58 +0200

    System Signature: 82e4c1e585625eb86742a29a590e8a9c0b747a54580e8431ad655abd870e8201

---
+++ control-panel/control-panel/pages/cp.security.signin.page.php
@@ -0,0 +1,40 @@
+<div class="moadal-full-page">
+    <div style="margin:1rem">
+        <div class="main-blog anim" style="--delay: 0.1s; max-width:25rem; margin: 15vh auto;background: #42414c;/* margin: 1rem; */">
+
+            <img src="/rescources/theme/oaklyn/Ice-Bear.png" class="anim" style="max-width: 11rem; display: block; margin: auto; padding: 1rem; filter:invert(1);">
+
+            <div class="main-blog__title" style="text-align: center; width:100%; max-width: fit-content; margin: auto;">
+                Control Panel Locked
+            </div>
+
+            <div class="anim" style="font-size: 10px; max-width: fit-content; margin: 1px 5px 20px 5px;">
+                To gain Access to your website control panel. Please login to your Trading Pivot to gain access.
+                <br>
+            </div>
+
+            <div class="anim" style="--delay: .4s; background:inherit; margin-bottom: 1.5rem;">
+                <div class="video-view" style="padding: 10px 20px 0px; background:inherit; ">Username</div>
+                <div class="video-name" style="background:inherit">
+                    <div class="search-bar">
+                        <input type="text" placeholder="Username" id="edt_username">
+                    </div>
+                </div>
+
+                <div class="video-view" style="padding: 10px 20px 0px; background:inherit; ">Password</div>
+                <div class="video-name" style="background:inherit">
+                    <div class="search-bar">
+                        <input type="password" placeholder="Password" id="edt_password">
+                    </div>
+                </div>
+
+                <button onclick="authenticate_website()" style="margin: 1rem auto; display: block;">
+                    Login
+                </button>
+            </div>
+
+        
+            <div class="main-blog__time">Security Authentication</div>
+        </div>
+    </div>
+</div>
