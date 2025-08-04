955c09cc749a5762bfbe8b6ca5332f9e9b0cc25f
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:42:09 +0200

    System Signature: 7414421be841aff79b3705f3e44e51902642e26b72cd111e856d4e57d98580ed

---
+++ control-panel/control-panel/pages/store.dashboard.page.php
@@ -0,0 +1,40 @@
+<div class="wrapper" style="overflow: auto">
+    <?php include_once "blade.navbar.sidebar.php"; ?>
+    <div class="main-container" id="application_canvas" style="overflow: visible">
+
+        <div class="main-header anim" style="--delay: 0s; text-align: center; padding: 1rem 3rem">
+            Welcome Back 
+        </div>
+
+        <style>
+            .intro_container{
+                background: linear-gradient(
+                    to right,
+                    rgb(0, 0, 0, 0.4),
+                    rgb(0, 0, 0, 0),
+                );
+            }
+        </style>
+        <div>
+            <div class="intro_container">
+                <div style="background-image: url(`https://cdn.varsitymarket.shop/img/timeline/cover-2024-11-arc.jpg`);"></div>
+                Welcome
+            </div>
+        </div>
+
+        <div class="main-blog anim" style="--delay: 0.1s; width: 100%; height: max-content; background-color: bisque; background: linear-gradient(181deg, #8e8e8f, transparent); margin: 1rem 0rem;">
+            <div class="main-blog__author">
+                <div class="author-img__wrapper">
+                    <img class="author-img" src="favicon.png">
+                </div>
+                <div class="author-detail">
+                    <div class="author-name" style="filter: blur(4px);">[USERNAME_PLACEHOLDER]</div>
+                    <div class="author-info" style="filter: blur(1px);">https://github.com/varsitymarket-tech/control-panel/</div>
+                </div>
+            </div>
+
+            <div class="main-blog__time">Github Control</div>
+        </div>
+
+    </div>
+</div>
