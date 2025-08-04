95a0b73f7a51cf8eeb775bfc1242e1afeb86fa54
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:00 +0200

    System Signature: ad9281a1850a330b9a904d2b5b2d3054cf0cf9f8e828bc7cd70272bd8fd31a40

---
+++ pages/become_a_vendor.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Become A Vendor', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Become A Vendor</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Marketplace Vendor
+            </h3>
+        </div>
+        <div class="flex-w flex-tr">
+            <div class="size-210  p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <img src="images/new_idea.gif" style="width:100%;">
+            </div>
+
+            <div class="size-210  flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <p class="cl5 ">If you wish to sell your products on the marketplace, register below and qualify to sell your store inventory.</p>
+                <div class="p-t-18">
+                    <a href="https://varsitymarket.store/" target="_blank">
+                        <button class="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
+                            Register
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
