a5fcb6f1e701073249e3d31e66193e76fc7c0782
VARSITYMARKET_TECHNOLOGIES <hastings@varsitymarket.tech>
2025-08-05 00:58:25 +0200

    System Signature: 484b79c1d3606193483b4141251e8909f696f3b6077aa55657dba98b028cf0e1

---
+++ pages/return_and_refund.php
@@ -0,0 +1,75 @@
+<?php
+
+include_once "top.php";
+echo (create_seo_signature('Return & Refund', '', 'SITE_OWNER', ''));
+
+include_once "header.php"; ?>
+
+<section class="bg0 p-t-104 p-b-116">
+    <div class="container">
+
+        <div class="p-b-45">
+            <p class="cl5 txt-center">Site Policies</p>
+            <h3 class="ltext-106 cl5 txt-center">
+                Return & Refund
+            </h3>
+        </div>
+
+        <div class="bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+            <p class="cl5 ">
+                Effective Date: 2024/03/06<br>
+                <br>
+                At Cross Gen, we want you to be completely satisfied with your purchase. If you encounter any issues with your order, we are here to assist you through our hassle-free return and refund process. This Return and Refund Policy outlines the procedures for returning products and seeking refunds. Please read this policy carefully to understand your rights and responsibilities regarding returns and refunds.<br>
+                <br>
+                <strong>1. Eligibility for Returns:</strong><br>
+                <br>
+                a. Defective or Damaged Products: If you receive a defective or damaged product, please contact our customer support team (refer to the Customer Support and Contact Information Policy) within [Insert Number] days of receiving the item.<br>
+                <br>
+                b. Incorrect Products: In the rare event that you receive the wrong product, please notify us within 0-3 days of delivery, and we will promptly address the issue.<br>
+                <br>
+                <strong>2. Return Process:</strong><br>
+                <br>
+                a. Authorization: Before returning any product, you must obtain authorization from our customer support team. They will guide you through the return process and provide you with a Return Merchandise Authorization (RMA) number.<br>
+                <br>
+                b. Original Condition: To be eligible for a return, the product must be in its original condition, unused, and in the original packaging, with all accessories and tags intact.<br>
+                <br>
+                c. Return Shipping: You are responsible for shipping the product back to us, unless the return is due to our error (defective, damaged, or incorrect product).<br>
+                <br>
+                <strong>3. Refund Process:</strong><br>
+                <br>
+                a. Evaluation: Once we receive the returned product, our team will inspect it to verify its condition and authenticity.<br>
+                <br>
+                b. Refund Method: Refunds will be issued using the original payment method used for the purchase. If the payment method is unavailable, we will arrange an alternative refund method in agreement with you.<br>
+                <br>
+                c. Refund Timing: Please allow 3-5 business days for the refund to be processed after we receive the returned product. The timing of the actual refund receipt may vary depending on your payment provider.<br>
+                <br>
+                <strong>4. Non-Returnable Items:</strong><br>
+                <br>
+                Certain products are non-returnable for hygiene and safety reasons. These may include, but are not limited to, the following:<br>
+                <br>
+                Personal care items (e.g., face masks, undergarments)<br>
+                Health and wellness products (e.g., supplements)<br>
+                Customized or personalized items<br>
+                Clearance or final sale items<br>
+                <strong>5. Policy Limitations:</strong><br>
+                <br>
+                a. Return Shipping Costs: If the return is not due to our error, the original shipping and return shipping costs are non-refundable.<br>
+                <br>
+                b. Partial Refunds: In some cases, a partial refund may be issued for products that are not in their original condition, have missing parts, or show signs of use.<br>
+                <br>
+                <strong>6. Policy Updates:</strong><br>
+                <br>
+                This Return and Refund Policy may be updated from time to time to reflect changes in our return and refund practices. Any revisions will be communicated on our website, and the updated policy will be effective from the date of posting.<br>
+                <br>
+                If you have any questions or need further clarification regarding our Return and Refund Policy, please don't hesitate to contact us.<br>
+                <br>
+                Thank you for shopping with Cross Gen!<br>
+            </p>
+
+        </div>
+
+
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
